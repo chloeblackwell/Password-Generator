000f6361a9fca22281e07bf723f179b43b0814ad
Chloe Blackwell <chloeblackwell2@hotmail.com>
2020-09-02 19:12:34 +1000

    (Updated Javascript file

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,14 @@
 // User Input variables 
 
-var button;
+var length;
 
-// Array of Special Characters 
+// Array's of Characters 
 
 var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 
-// Array of Numeric Charaters 
-
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Array of Lower Characters 
-
 var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-// Array of Upper Characters 
 
 var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
@@ -24,9 +18,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordNode = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordNode.value = password;
 
 }
 
@@ -38,31 +32,68 @@ function generatePassword() {
   
   // User input 
 
-  button = prompt ("How many characters would you like? Choose between 8 and 128 characters");
+  length = prompt ("How many characters would you like? Choose between 8 and 128 characters");
 
   // If Else Statement to generate password 
 
-  if (!button) {
+  if (!length) {
   alert ("This must have a value!")
-  }  else if (button < 8 || button > 128) {
-    button = prompt ("You must choose between 8 and 128!");
+  }  else if (length < 8 || length > 128) {
+    length = prompt ("You must choose between 8 and 128!");
+    
   } else {
 
-    specialCharacters = confirm("Will your password contain special characters?");
-    numericCharacters = confirm("Will your password contain numbers?");
-    lowerCasedCharacters = confirm("Will your password contain lower cased characters?");
-    upperCasedCharacters = confirm("Will your password contain upper cased characters?");
+    var isSpecial = confirm("Will your password contain special characters?");
+    var isNumeric = confirm("Will your password contain numbers?");
+    var isLower = confirm("Will your password contain lower cased characters?");
+    var isUpper = confirm("Will your password contain upper cased characters?");
   };
 
   // If Else statement for when user cancels all 
 
-  if (!specialCharacters && !numericCharacters && !lowerCasedCharacters && !upperCasedCharacters) {
-  alert ("You must choose a criteria"); 
+  if (!isSpecial && !isNumeric && !isLower && !isUpper) {
+  passwordText = alert ("You must choose a criteria"); 
   } 
+ 
+  // If Else statement for all options selected 
+
+  else if (isSpecial && isNumeric && isLower && isUpper) {
+    
+    passwordText = specialCharacters.concat(numericCharacters, lowerCasedCharacters, upperCasedCharacters);
+
+   // If Else statement for all other options that are avaliable 
+
+  } else if (isSpecial && isNumeric && isLower) {
+
+   passwordText = specialCharacters.concat(numericCharacters, lowerCasedCharacters);
+
+  } else if (isSpecial && isNumeric && isUpper) {
+
+    passwordText = specialCharacters.concat(numericCharacters, upperCasedCharacters);
+
+  } else if (isNumeric && isLower && isUpper) {
+
+    passwordText = numericCharacters.concat(lowerCasedCharacters, upperCasedCharacters);
+  } 
+
+
+
+  var password = [ ];  
+  // Random generation for password 
+  for ( var i = 0; i < length; i++) {
+    var random = passwordText[Math.floor(Math.random() * passwordText.length)];
+    password.push(random);
+  } 
+  return password.join('');
+
+  
+
+
+
 
 }
 
 
 
-// If else statements for the different options avaliable 
+
 
